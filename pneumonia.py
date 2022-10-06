d9b14917e706c799071fd6a68b5fea641797d4cb
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-10-06 08:21:52 -0700

    Adapt lung segmentation

--- pneumonia.py
+++ pneumonia.py
@@ -28,7 +28,7 @@ from imageai.Detection.Custom import DetectionModelTrainer
 import tensorflow as tf
 from tqdm import tqdm, tqdm_notebook
 from skimage import morphology
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 cv2.__version__
 
@@ -58,8 +58,13 @@ file_names_pneumonia = [path[bddl:] for path in glob.glob(base_data_dir + 'train
                                                                                                             '../data/chest_xray/test/PNEUMONIA/*.jpeg')] + [
                            path[bddl:] for path in glob.glob('../data/chest_xray/val/PNEUMONIA/*.jpeg')]
 
-
 # +
+from keras.models import load_model
+
+model_name = '/Users/zhuvikin/workspace/lung-segmentation-2d/trained_model.hdf5'
+UNet = load_model(model_name)
+
+
 def adjust_gamma(image, gamma = 1.0):
     invGamma = 1.0 / gamma
     table = np.array([
@@ -82,17 +87,30 @@ def normalize_image(original, target_width = 256):
     im_shape = (256, 256)
     original_width = original.shape[1]
     original_height = original.shape[0]
-    
+
     width_coeff = 256 / original_width
     height_coeff = 256 / original_height
-    
+
     img = cv2.resize(original, im_shape, interpolation = cv2.INTER_AREA)
     img = cv2.equalizeHist(img)
-    img = adjust_gamma(img, 0.7)
-    img = adjust_contrast(img, 3)
+    img = adjust_gamma(img, 0.75)
+    img = img * 1.2
+    
+
+    #     mask=np.zeros((256, 256),dtype=np.uint8)
+    #     cv2.rectangle(mask,(0,0),(255,255),(255,255,255),10)
+    #     mask = cv2.GaussianBlur(mask, (33, 33), cv2.BORDER_REFLECT101)
+    #     img = img + mask
+
+    img = np.clip(img, 0, 255)
+#     img = cv2.equalizeHist(img)
+    
+    #     img = adjust_contrast(img, 3)
 
     img2 = img.astype(float)
-    img2 = scaler.fit_transform(img2) / 3
+    img2 -= img2.mean()
+    img2 /= img2.std()
+
     img2 = np.expand_dims(img2, -1)
     inp_shape = img2.shape
     pred = UNet.predict([[img2]])[..., 0].reshape(inp_shape[:2])
@@ -102,25 +120,25 @@ def normalize_image(original, target_width = 256):
 
     non_empty_columns = np.where(pr.max(axis = 0) > 0)[0]
     non_empty_rows = np.where(pr.max(axis = 1) > 0)[0]
-    left = min(non_empty_columns)
-    right = max(non_empty_columns)
-    top = min(non_empty_rows)
-    bottom = max(non_empty_rows)
+    left = min(non_empty_columns) if len(non_empty_columns) > 0 else 0
+    right = max(non_empty_columns) if len(non_empty_columns) > 0 else 256
+    top = min(non_empty_rows) if len(non_empty_rows) > 0 else 0
+    bottom = max(non_empty_rows) if len(non_empty_rows) > 0 else 256
 
     left_r = left / 256
     right_r = right / 256
     top_r = top / 256
     bottom_r = bottom / 256
-    
+
     l = floor(left / width_coeff)
     t = floor(top / height_coeff)
     w = floor((right - left) / width_coeff)
     h = floor((bottom - top) / height_coeff)
-    
+
     cropped = original[t:t + h, l:l + w]
     resized = cv2.resize(cropped, (target_width, target_width), interpolation = cv2.INTER_AREA)
     equalized = cv2.equalizeHist(resized)
-    return equalized
+    return 256 * pr # img # equalized #
 
 
 def remove_small_regions(img, size):
@@ -130,20 +148,24 @@ def remove_small_regions(img, size):
     return img
 
 
-# -
+# +
+pb = [1073, 1261, 1289, 1297, 1412, 1417, 1464, 3906, 321, 371, 426, 544, 581, 694, 749, 883, 1220, 1225, 1586,
+      1700, 1707, 1777, 1832, 1859, 1969, 1999, 2163, 2497, 2642, 2741, 2778, 3009, 3076, 3187, 3540, 3694,
+      3774]
 
-file_names_pneumonia[:10]
+# list(map(lambda i: file_names_pneumonia[i], pb))
 
 # +
-i = 0
-for path in tqdm_notebook(file_names_normal[:10]):
-    img = cv2.imread(base_data_dir + path, 0)
-    normalized = normalize_image(img, 256)
-    cv2.imwrite(normal_dir + '{0:04d}.jpeg'.format(i), normalized)
-    i += 1
+# i = 0
+# for path in tqdm_notebook(file_names_normal):
+#     img = cv2.imread(base_data_dir + path, 0)
+#     normalized = normalize_image(img, 256)
+#     cv2.imwrite(normal_dir + '{0:04d}.jpeg'.format(i), normalized)
+#     i += 1
+
 
 i = 0
-for path in tqdm_notebook(file_names_pneumonia[:20]):
+for path in tqdm_notebook(list(map(lambda i: file_names_pneumonia[i], pb))): # tqdm_notebook(file_names_pneumonia):
     img = cv2.imread(base_data_dir + path, 0)
     normalized = normalize_image(img, 256)
     if 'bacteria' in path:
@@ -152,71 +174,6 @@ for path in tqdm_notebook(file_names_pneumonia[:20]):
         cv2.imwrite(virus_dir + '{0:04d}.jpeg'.format(i), normalized)
     i += 1
 
-# +
-from keras.models import load_model
-
-model_name = '/Users/zhuvikin/workspace/lung-segmentation-2d/trained_model.hdf5'
-UNet = load_model(model_name)
-
-# +
-fig = plt.figure(figsize = (17, 6))
-i = 1
-
-scaler = StandardScaler()
-
-for problem in tqdm(file_names_pneumonia[100:118]): # problems[18:36]: 
-    im_shape = (256, 256)
-    img_path = base_data_dir + problem
-    original = cv2.imread(img_path, 0)
-    original_width = original.shape[1]
-    original_height = original.shape[0]
-    
-    width_coeff = 256 / original_width
-    height_coeff = 256 / original_height
-    
-    img = cv2.resize(original, im_shape, interpolation = cv2.INTER_AREA)
-    img = cv2.equalizeHist(img)
-    img = adjust_gamma(img, 0.7)
-    img = adjust_contrast(img, 3)
-
-    img2 = img.astype(float)
-    img2 = scaler.fit_transform(img2) / 3
-    img2 = np.expand_dims(img2, -1)
-    inp_shape = img2.shape
-    pred = UNet.predict([[img2]])[..., 0].reshape(inp_shape[:2])
-    pr = pred > 0.5
-    pr = remove_small_regions(pr, 0.01 * np.prod(im_shape))
-    pr = pr.astype(int)
-
-    non_empty_columns = np.where(pr.max(axis = 0) > 0)[0]
-    non_empty_rows = np.where(pr.max(axis = 1) > 0)[0]
-    left = min(non_empty_columns)
-    right = max(non_empty_columns)
-    top = min(non_empty_rows)
-    bottom = max(non_empty_rows)
-
-    left_r = left / 256
-    right_r = right / 256
-    top_r = top / 256
-    bottom_r = bottom / 256
-    
-    l = floor(left / width_coeff)
-    t = floor(top / height_coeff)
-    w = floor((right - left) / width_coeff)
-    h = floor((bottom - top) / height_coeff)
-    
-    cropped = original[t:t + h, l:l + w]
-    resized = cv2.resize(cropped, (256, 256), interpolation = cv2.INTER_AREA)
-    equalized = cv2.equalizeHist(resized)
-
-    ax = plt.subplot(3, 6, i)
-    plt.axis('off')
-    plt.imshow(equalized, cmap = 'gray')
-
-    i += 1
-
-plt.subplots_adjust(wspace = 0.03, hspace = 0.03)
-plt.show()
 # -
 
 
